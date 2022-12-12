46aee8db16953a168c9d08c884b73925718fd4f6
ekkim <eklikeable@kakao.com>
2022-12-12 23:50:12 +0900

    2022.12.11 수업내용

--- 221210/learn-react/src/App.js
+++ 221210/learn-react/src/App.js
@@ -1,5 +1,7 @@
-import "./App.css"; // webpack을 통해서 javaScript가 아닌 파일도 import가 가능
-import TodoInput from "./components/TodoInput";
+import './App.css'; // webpack을 통해서 javaScript가 아닌 파일도 import가 가능
+import React, { useState, useEffect } from 'react';
+
+import Todo from './components/Todo';
 
 function App() {
   /* 
@@ -13,14 +15,40 @@ function App() {
     6. 컴포넌트는 무조건 대괄호로 시작한다.
   */
 
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    // 렌더링이 일어날 때마다 실행되는 코드
+    console.log('렌더링');
+  });
+
+  useEffect(() => {
+    // []안의 값에 변화가 있을 때에만 실행된다.
+    console.log('count : ', count);
+  }, [count]);
+
+  useEffect(() => {
+    console.log('input : ', input);
+  }, [input]);
+
+  const handleCount = () => {
+    setCount(count + 1);
+  };
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div>
-      {/* <Hello text="seok" active={true} />
-      <p style={wordStyle} className="content">
-        안녕하세요, {myName}!
-      </p> */}
-      <TodoInput />
-    </div>
+    <>
+      <Todo />
+    </>
   );
 }
 
--- 221210/learn-react/src/components/Counter.jsx
+++ 221210/learn-react/src/components/Counter.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0);
   // 상태값 : 컴포넌트가 가지는 상태값 => 상태가 변하면 렌더링이 다시 일어난다.
   // useState(초기값) : [상태값, 업데이트함수]
   //  => useState는 비동기적으로 동작한다
+  //  => 함수형 업데이트 : useState의 인자로 함수를 전달하면 매개변수에 최신상태이 담긴다.
 
   const handleCount = (opNum) => {
     if (opNum < 0 && count <= 0) return;
